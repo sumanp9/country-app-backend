37212a56659e849157668920dd136c7fed7ff292
suman <11.sumanpradhan@gmail.com>
2023-09-07 21:46:25 -0500

    update and delete api calls

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/hello', function(req, res) {
 });
 
 app.get('/api/getCountries', function(req, res) {
-    const query = `SELECT country_name FROM countries`;
+    const query = `SELECT id, country_name FROM countries`;
 
     database.query(query, (err, results) => {
         if(err) {
@@ -36,7 +36,11 @@ app.get('/api/getCapitalCity/:countryName', function(req, res) {
             res.status(500).json({ error: 'Error executing query to get capital city'});
         
         } else {
-            res.json(results);
+            if(results.length > 0) {
+                res.json(results[0]);
+            } else {
+            res.json({capital_city: ''});
+            }
         }
     });
 });
@@ -58,6 +62,38 @@ app.post('/api/addCountryDetails', function(req, res) {
         }
     });  
 
+})
+
+app.put('/api/updateCountry', function(req, res){
+    const countryData = req.body;
+    const query = `UPDATE countries SET country_name=?, capital_city=? Where id=?`;
+    const values = [countryData.country_name, countryData.capital, countryData.id];
+
+
+    database.query(query, values, (err, results) => {
+        if(err) {
+            console.error('Error executing query to update country details:', err);
+            res.status(500).json({ error: 'Error executing query to update country details'});
+        } else {
+            console.log('Country updated successfully');
+            res.status(201).json({message: 'Country object recieved and updated successfully'})
+        }
+    })
+})
+
+app.delete('/api/deleteCountryData/:countryName', function(req, res) {
+    const countryName = req.params.countryName;
+    const query = `DELETE FROM countries where country_name=?`;
+    database.query(query, [countryName], (err, results) => {
+
+        if(err){
+            console.log(err);
+            res.status(500).json({error: 'Error executing query to delete'});
+        } else{
+            res.status(204).json();
+        }
+
+    })
 })
 
 var server = app.listen(8081, function() {
