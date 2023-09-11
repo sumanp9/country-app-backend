ebd54204d44dbccafa09bdb89632b48d79c8270f
suman <11.sumanpradhan@gmail.com>
2023-09-11 12:17:22 -0500

    updated API names

--- server.js
+++ server.js
@@ -7,13 +7,9 @@ var database = require('./connection.js');
 app.use(cors());
 app.use(express.json());
 
+// Update the api signature accoriding to swagger ui's rest endpoints
 
-app.get('/hello', function(req, res) {
-    console.log("Sharing hello");
-    res.end("Hello world how are we");
-});
-
-app.get('/api/getCountries', function(req, res) {
+app.get('/api/countries', function(req, res) {
     const query = `SELECT id, country_name FROM countries`;
 
     database.query(query, (err, results) => {
@@ -27,7 +23,7 @@ app.get('/api/getCountries', function(req, res) {
 });
 
 
-app.get('/api/getCapitalCity/:id', function(req, res) {
+app.get('/api/capital/:id', function(req, res) {
     const id = req.params.id;
     const query = `SELECT capital_city FROM countries where id=?`;
     database.query(query,[id], (err, results) => {
@@ -45,10 +41,25 @@ app.get('/api/getCapitalCity/:id', function(req, res) {
     });
 });
 
+app.get('/api/cities/:id', function(req, res) {
+    const country_id = req.params.id;
+    const query = `SELECT id, name from cities where country_id =?`;
+    database.query(query, [country_id], (err, results) => {
+        if(err) {
+            console.error(err);
+            res.status(500).json({error: 'Error executing query to retireve  cities'});
+        } else {
+            res.json(results);
+        }
+    })
+})
 
-app.post('/api/addCountryDetails', function(req, res) {
+
+app.post('/api/countrydetails', function(req, res) {
     const countryData = req.body;
     const query = `INSERT INTO Countries (country_name, capital_city) VALUES (?,?)`;
+
+  //  this.validateCountry(countryData);
     const values = [countryData.country_name, countryData.capital];
 
     database.query(query, values,  (err, results) => {
@@ -63,7 +74,7 @@ app.post('/api/addCountryDetails', function(req, res) {
 
 })
 
-app.put('/api/updateCountry', function(req, res){
+app.put('/api/country', function(req, res){
     const countryData = req.body;
     const query = `UPDATE countries SET country_name=?, capital_city=? Where id=?`;
     const values = [countryData.country_name, countryData.capital, countryData.id];
@@ -80,7 +91,7 @@ app.put('/api/updateCountry', function(req, res){
     })
 })
 
-app.delete('/api/deleteCountryData/:id', function(req, res) {
+app.delete('/api/country/:id', function(req, res) {
     const countryId = req.params.id;
     const query = `DELETE FROM countries where id=?`;
     database.query(query, [countryId], (err, results) => {
@@ -95,6 +106,57 @@ app.delete('/api/deleteCountryData/:id', function(req, res) {
 
     })
 })
+
+app.post('/api/city', function(req, res) {
+    const countryId = req.body.countryId;
+    const cityName = req.body.cityName;
+    const cityId = req.body.id;
+
+    if(cityId == 0) {
+        const query=`INSERT INTO Cities (country_id, name) VALUES (?,?)`;
+        const values = [countryId, cityName];
+        database.query(query, values,  (err, results) => {
+            if(err) {
+                console.error('Error executing query to add city details:', err);
+                res.status(500).json({ error: 'Error executing query to add city details'});
+            } else {
+                console.log('City added successfully');
+                res.status(201).json({message: 'City object recieved and processed successfully'})
+            }
+        });  
+
+    } else {
+        const query = `UPDATE cities SET name=? Where id=?`;
+    const values = [cityName, cityId];
+
+
+    database.query(query, values, (err, results) => {
+        if(err) {
+            console.error('Error executing query to update city details:', err);
+            res.status(500).json({ error: 'Error executing query to update city details'});
+        } else {
+            console.log('City updated successfully');
+            res.status(201).json({message: 'City object recieved and updated successfully'})
+        }
+    })
+    }
+})
+
+app.delete('/api/city/:cityId', function(req, res) {
+    const cityId = req.params.cityId;
+
+    const query = "DELETE FROM Cities Where id=?";
+    database.query(query, [cityId], (err, results) => {
+        if(err){
+            console.log(err);
+            res.status(500).json({error: 'Error executing query to delete'});
+        } else{
+            console.log("delted country with id: "+  cityId)
+            res.status(204).json();
+        }
+    })
+})
+
 
 var server = app.listen(8081, function() {
     var host = server.address().address;
